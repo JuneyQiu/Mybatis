03a9430b8312cbb2d4a0d681965223007d034651
chen-derek <chenhaijing1314@163.com>
2017-06-11 13:46:14 +0800

    first submit

---
+++ src/main/java/cn/springmvc/service/imp/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.springmvc.service.imp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.springmvc.dao.UserDAO;
+import cn.springmvc.model.User;
+import cn.springmvc.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserDAO userDAO;
+	
+	@Override
+	public int insertUser(User user) {
+		// TODO Auto-generated method stub
+		return userDAO.insertUser(user);
+	}
+}
